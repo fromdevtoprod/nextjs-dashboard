34e0aa79e36b86e5a725fbcf10066098988c19df
Cédric Ribalta <cedric.ribalta@protonmail.com>
2024-07-21 22:33:14 +0200

    feat: implement customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,30 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import { Suspense } from 'react';
+import { Metadata } from 'next';
+import Table from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import { lusitana } from '@/app/ui/fonts';
+import Search from '@/app/ui/search';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: { query?: string; page?: number };
+}) {
+  const query = searchParams.query || '';
+  const filteredCustomers = await fetchFilteredCustomers(query);
+  return (
+    <div className="w-full">
+      <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
+        Customers
+      </h1>
+      <Search placeholder="Search customers..." />
+      <Suspense fallback={<div>Loading....</div>}>
+        <Table customers={filteredCustomers} />
+      </Suspense>
+    </div>
+  );
 }
