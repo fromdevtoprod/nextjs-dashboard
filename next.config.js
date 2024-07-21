5f804db3c2bd7866e0b052a386658e26a73ccf50
Cédric Ribalta <cedric.ribalta@protonmail.com>
2024-07-21 15:39:49 +0200

    feat: disable experimental ppr

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    ppr: 'incremental',
-  },
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental: {
+//     ppr: 'incremental',
+//   },
+// };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
