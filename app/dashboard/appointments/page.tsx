84d4ce7299946393318c3041d50324242bdd6e7b
Cédric Ribalta <cedric.ribalta@protonmail.com>
2024-08-14 12:06:24 +0200

    feat: trying new calendar for appointments

--- app/dashboard/appointments/page.tsx
+++ app/dashboard/appointments/page.tsx
@@ -1,8 +1,11 @@
+import Calendar from '@/app/ui/calendar';
+
 export default function Page() {
   return (
     <div className="flex h-screen flex-col items-center justify-center">
       <h1 className="text-4xl font-bold">Appointments</h1>
       <p className="text-lg text-gray-500">Coming soon...</p>
+      <Calendar />
     </div>
   );
 }
