e9ea799cf54ae3faca167141b9a8a7b6b742ea99
Cédric Ribalta <cedric.ribalta@protonmail.com>
2024-10-10 16:39:42 +0200

    fix: detect browser language on server side

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,12 +1,34 @@
+import { headers } from 'next/headers';
 import { getRequestConfig } from 'next-intl/server';
 
+const frSupportedLocales = [
+  'fr',
+  'fr-FR',
+  'fr-CA',
+  'fr-BE',
+  'fr-CH',
+  'fr-LU',
+  'fr-MC',
+];
+
 export default getRequestConfig(async () => {
+  const acceptLanguageHeaders = headers().get('accept-language');
+  const languages = acceptLanguageHeaders?.split(';');
   // Provide a static locale, fetch a user setting,
   // read from `cookies()`, `headers()`, etc.
-  const locale = 'en';
+  let locale = 'en';
+  if (languages && isFrenchDetected(languages)) {
+    locale = 'fr';
+  }
 
   return {
     locale,
     messages: (await import(`../../messages/${locale}.json`)).default,
   };
 });
+
+function isFrenchDetected(languages: string[]) {
+  return languages?.some((language) =>
+    frSupportedLocales.some((frLocale) => language.includes(frLocale)),
+  );
+}
