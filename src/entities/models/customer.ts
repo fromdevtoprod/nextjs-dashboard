0a5d1509dcee4009449b97aff92eb156b509bcfc
Cédric Ribalta <cedric.ribalta@protonmail.com>
2024-10-18 17:23:45 +0200

    fix: remove zod

--- src/entities/models/customer.ts
+++ src/entities/models/customer.ts
@@ -1,16 +1,12 @@
-import { z } from 'zod';
-
-export const customer = z.object({
-  address: z.string().nullable(),
-  birth_date: z.string(),
-  city: z.string().nullable(),
-  created_at: z.date().nullable(),
-  email: z.string(),
-  id: z.string(),
-  name: z.string(),
-  pathology: z.string().nullable(),
-  phone: z.string(),
-  postal_code: z.string().nullable(),
-});
-
-export type Customer = z.infer<typeof customer>;
+export type Customer = {
+  id: string;
+  name: string;
+  email: string;
+  phone: string;
+  birth_date: string;
+  address: string | null;
+  city: string | null;
+  postal_code: string | null;
+  pathology: string | null;
+  created_at: Date | null;
+};
